0a2ab03d96f399ac58e514d963439fbcc92b73d9
Sajal Gupta <53595953+sajal243@users.noreply.github.com>
2020-05-26 08:17:23 +0530

    Update BONUS.py

--- day3/BONUS.py
+++ day3/BONUS.py
@@ -1,15 +1,14 @@
 lst1 = list(map(int, input("Enter the list1 items:").split()))
-maxx = sum(lst1)
 lst2 =[]
-
+n=len(lst1)
 from itertools import combinations
-for i in range(2,max(lst1)+1):
+for i in range(2,2**n + 1):
     comb= combinations(lst1,i)
     for i in list(comb):
         lst2.append(sum(i))
 
 
-for i in range(1,maxx):
+for i in range(1,2**n+1):
     if ((i not in lst1) and (i not in lst2)):
         print(i)
         break
